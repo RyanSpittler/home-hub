9accff82af65dc7924d3c2e7bea4b528070a2cca
Recreation <recreation@Ryans-MacBook-Pro.local>
2017-10-14 17:28:24 -0600

    Accept input, change SoundBar
    - This will accept secret, key, and text.
    - It will authenticate the credentials, simply.
    - It will check the current input of the SoundBar.
    - It will set the input to the string passed.
    - The string passed is not validated, so it should be whatever the Django REST API on the SoundBar is expecting.

--- lambda.js
+++ lambda.js
@@ -1,11 +1,8 @@
 var config = require('./config.json');
-// var weather = require('./lib/weather.js');
 var soundBar = require("./lib/soundBar.js");
 
 // Entrypoint for AWS Lambda
 exports.handler = function(event, context) {
-  var zipCode = event.text ? event.text.trim() : null;
-
   console.log('[EVENT] ', event);
 
   // basic auth
@@ -13,23 +10,14 @@ exports.handler = function(event, context) {
     return context.fail('Unauthorized request. Check authentication credentials.');
   }
 
-  // validate zip code
-  if(!isValidZip(zipCode)) {
-    return context.fail('Must be a valid 5 digit zip code.');
-  }
-
-  // get weather && respond to slack request
-  weather.byZip(zipCode).then(function(response) {
+  // get input && respond
+  soundBar.setInput(event.text).then(function(response) {
     context.succeed(response);
   }).catch(function(reason) {
-    console.log('[FAIL] Unable to get weather: ', reason);
-    context.fail('Unable to get weather. Try again later.');
+    console.log('[FAIL] Unable to get current input: ', reason);
+    context.fail('Unable to get input. Try again later.');
   });
 };
-
-function isValidZip(zip) {
-  return zip && zip.length === 5 && !isNaN(zip);
-}
 
 function isValidAuth(secret, key) {
   return secret && key && secret === config.auth.secret && key === config.auth.key;
